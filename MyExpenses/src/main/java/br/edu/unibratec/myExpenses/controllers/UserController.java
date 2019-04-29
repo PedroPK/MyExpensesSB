ea4e20bcb1d3fbf3e0fada928837570f09e0e150
pedropk <pedropk@gmail.com>
2019-04-29 14:19:35 -0300

    Added a method to do a Initial Load at Users table

--- MyExpenses/src/main/java/br/edu/unibratec/myExpenses/controllers/UserController.java
+++ MyExpenses/src/main/java/br/edu/unibratec/myExpenses/controllers/UserController.java
@@ -1,5 +1,7 @@
 package br.edu.unibratec.myExpenses.controllers;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -7,6 +9,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
+import br.edu.unibratec.myExpenses.model.entities.User;
 import br.edu.unibratec.myExpenses.model.repositories.UserRepository;
 
 @RestController
@@ -26,6 +29,15 @@ public class UserController {
 		mav.addObject( "users", aRepository.findByName(pName));
 		
 		return mav;
+	}
+	
+	@RequestMapping("/initialLoad")
+	public String inicialLoadUser() {
+		User user = new User("Johnny", "johnny@is.com", "Planet of Apes");
+		
+		this.aRepository.save(user);
+		
+		return "The Default User was Saved in the Database.";
 	}
 	
 }
