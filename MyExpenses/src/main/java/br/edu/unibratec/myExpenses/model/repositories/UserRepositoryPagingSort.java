4c46952c830353c2bc0f02b0121a42a2c9fd317c
pedropk <pedropk@gmail.com>
2019-04-30 08:24:12 -0300

    Renaming the Repository, appending inheritance interface name

--- MyExpenses/src/main/java/br/edu/unibratec/myExpenses/model/repositories/UserRepositoryPagingSort.java
+++ MyExpenses/src/main/java/br/edu/unibratec/myExpenses/model/repositories/UserRepositoryPagingSort.java
@@ -5,10 +5,12 @@ import java.util.List;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
 import br.edu.unibratec.myExpenses.model.entities.User;
 
-public interface UserRepository extends PagingAndSortingRepository<User, Long> {
+@Repository			// Once it extends a Spring Data Repository Interface, this annotation is just didactic, informational and optional. 
+public interface UserRepositoryPagingSort extends PagingAndSortingRepository<User, Long> {
 	
 	// Manually implemented methods
 	
