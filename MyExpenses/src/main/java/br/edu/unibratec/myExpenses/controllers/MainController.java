f1434071363cadec11e471b19330d59f9b73125f
pedropk <pedropk@gmail.com>
2019-05-06 14:13:51 -0300

    Redirecting to Thymeleaf Templates sucessfully
    I wasn't capable to make it work while returning just Strings when I was
    trying to access it using Localhost:9000 URL.
    
    Since I changed the Return Type to ModelAndView, its working now

--- MyExpenses/src/main/java/br/edu/unibratec/myExpenses/controllers/MainController.java
+++ MyExpenses/src/main/java/br/edu/unibratec/myExpenses/controllers/MainController.java
@@ -1,16 +1,33 @@
 package br.edu.unibratec.myExpenses.controllers;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
 import br.edu.unibratec.myExpenses.utils.UtilDateTime;
 
 @RestController
+@RequestMapping("/")
 public class MainController {
 	
-	@RequestMapping("/")
-	public String index() {
+	@GetMapping("/")
+	public String home() {
 		return "Welcome to inicial page of My Expenses App" + "<br>" + UtilDateTime.getDataHoraHojeAgora();
+	}
+	
+	@GetMapping("/layout")
+	public ModelAndView layout() {
+		ModelAndView mv = new ModelAndView("layout");
+		
+		return mv;
+	}
+	
+	@GetMapping("/index")
+	public ModelAndView index() {
+		ModelAndView mv = new ModelAndView("index");
+		
+		return mv;
 	}
 	
 }
